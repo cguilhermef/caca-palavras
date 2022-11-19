0ba5fc032f9f32cf1bc34117f71b7eb4b756115e
Christian G. Frtsch <christiangfritsch@gmail.com>
2022-11-18 23:14:26 -0300

    feat(terminal): finaliza layout e funcionalidades básicas

--- caca-palavras-terminal/caca-palavras-terminal.ino
+++ caca-palavras-terminal/caca-palavras-terminal.ino
@@ -3,15 +3,16 @@
 #include <U8g2lib.h>
 
 
-U8G2_SSD1306_128X64_NONAME_1_HW_I2C oled(/* U8G2_R2 */ U8G2_R0, SCL,  SDA, U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C oled(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 Button buttonA(9);
 Button buttonB(8);
 
+int step = 1;
 int dialer = A0;
-int maxDialerInput = 0;
-int minDialerInput = 0;
-int dialerDivider = 37;
-char command[5] = "\0";
+int selectedTeam = 0;
+int charCount = 0;
+char lastChar = "\0";
+char command[4] = "\0";
 
 void setup() {
   Serial.begin(4800);
@@ -26,28 +27,137 @@ void loop() {
   buttonA.read();
   buttonB.read();
 
-  char tmp[30];
-  sprintf(tmp, "Dialer: %d", analogRead(dialer));
-  if(buttonA.wasPressed()){
-    // strcpy(buffer, "A pressed!");
+  if (buttonA.pressedFor(3000)) {
+    if (step != 1) {
+      step = 1;
+      Serial.print("tm:rst#");
+      delay(10);
+    }
+    return;
   }
 
-  if(buttonB.wasPressed()){
-    // strcpy(buffer, "B pressed!");
-    
-    int read = analogRead(dialer);
-    read = read / dialerDivider;
-    read = read > 25 ? 25 : read;
-    char character = read + 65;
-    sprintf(command,"%c%c%cx", 'W', 'A', character);
-    Serial.print(command);
-    delay(10);
+  if (buttonA.wasReleased()) {
+    if (step == 2 && charCount > 0) {
+      Serial.print("tp:del#");
+      charCount--;
+      delay(10);
+      return;
+    }
   }
+
+  if (buttonB.wasPressed()) {
+    if (step == 1) {
+      int read = analogRead(dialer);
+      read = read / 83;
+      read = read > 12 ? 12 : read;
+      step = 2;
+      sprintf(command, "tm:%d#", read);
+      Serial.print(command);
+      delay(10);
+      return;
+    }
+    if (step == 2) {
+      sprintf(command, "tp:%c", readCharacter());
+      Serial.print(command);
+      charCount++;
+      delay(10);
+      return;
+    }
+  }
+  if (step == 1) {
+    showChooseTeam();
+  }
+  if (step == 2) {
+    showTyping();
+  }
+}
+
+void showChooseTeam() {
 
   oled.firstPage();
   do {
-    oled.setFont(u8g2_font_mercutio_sc_nbp_t_all);
-    oled.drawStr(10, 10, command);
-    oled.drawStr(10, 30, tmp);
+    drawFrame();
+    drawFrameTitle("Selecione a equipe");
+    oled.setFont(u8g2_font_helvR18_tf);
+    char * tmp = teamName();
+    int w = oled.getUTF8Width(tmp);
+    oled.drawUTF8((int)(128-w)/2, 50, tmp);
   } while (oled.nextPage());
+}
+
+void showTyping() {
+  oled.firstPage();
+  do {
+    drawFrame();
+    drawFrameTitle("Soletre a palavra");
+    oled.setFont(u8g2_font_helvR18_tf);
+    char tmp[2] = "";
+    sprintf(tmp, "%c", readCharacter());
+    int w = oled.getStrWidth(tmp);    
+    oled.drawStr((int)(128-w)/2, 50, tmp);
+  } while (oled.nextPage());
+}
+
+void drawFrameTitle(char* title) {
+  oled.setFont(u8g2_font_mercutio_sc_nbp_t_all);
+  oled.drawStr(6, 15, title);
+}
+
+void drawFrame() {
+  oled.drawRFrame(0, 0, 128, 64, 4);
+  oled.drawRFrame(1, 1, 126, 62, 4);
+}
+
+char * teamName() {
+  switch(readTeam()) {
+    case 0: {
+      return "Boi";
+    }
+    case 1: {
+      return "Cão";
+    }
+    case 2: {
+      return "Carneiro";
+    }
+    case 3: {
+      return "Cavalo";
+    }
+    case 4: {
+      return "Coelho";
+    }
+    case 5: {
+      return "Dragão";
+    }
+    case 6: {
+      return "Galo";
+    }
+    case 7: {
+      return "Macaco";
+    }
+    case 8: {
+      return "Porco";
+    }
+    case 9: {
+      return "Rato";
+    }
+    case 10: {
+      return "Serpente";
+    }
+    case 11: {
+      return "Tigre";
+    }
+  }
+}
+
+int readTeam() {
+  int read = analogRead(dialer);
+  read = read / 90;
+  return read > 11 ? 11 : read;
+}
+
+char readCharacter() {
+  int read = analogRead(dialer);
+  read = read / 40;
+  read = read > 25 ? 25 : read;
+  return read + 65;
 }
